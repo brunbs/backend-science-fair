82b266d17d1d040bd8d0555c8e652f102887b190
brunbs <babosa227@gmail.com>
2023-03-14 14:00:49 -0300

    cria esqueleto do projeto

---
+++ src/main/java/com/school/science/fair/Application.java
@@ -0,0 +1,13 @@
+package com.school.science.fair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
