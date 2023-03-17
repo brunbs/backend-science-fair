c087c47c4d3c6907dab9542cb3ff8b063cde0192
brunbs <babosa227@gmail.com>
2023-03-17 15:55:50 -0300

    SF-17 - add unit tests for class endpoints

--- src/test/java/com/school/science/fair/domain/mother/ClassMother.java
+++ src/test/java/com/school/science/fair/domain/mother/ClassMother.java
@@ -1,9 +1,13 @@
 package com.school.science.fair.domain.mother;
 
+import com.school.science.fair.domain.ClassResponse;
+import com.school.science.fair.domain.CreateClassRequest;
+import com.school.science.fair.domain.UpdateClassRequest;
 import com.school.science.fair.domain.dto.ClassDto;
 import com.school.science.fair.domain.dto.ClassRequestDto;
 import com.school.science.fair.domain.entity.Class;
 import com.school.science.fair.domain.enumeration.GradeYearEnum;
+import org.hibernate.sql.Update;
 
 public class ClassMother {
 
@@ -35,6 +39,28 @@ public class ClassMother {
     public static ClassRequestDto getUpdateClassRequestDto() {
         return ClassRequestDto.builder()
                 .name("Class B")
+                .build();
+    }
+
+    public static UpdateClassRequest getUpdateClassRequest() {
+        return UpdateClassRequest.builder()
+                .name("New name")
+                .build();
+    }
+
+    public static CreateClassRequest getCreateClassRequest() {
+        return CreateClassRequest.builder()
+                .name("Class A")
+                .gradeYear(CreateClassRequest.GradeYearEnum.EM_1ANO)
+                .build();
+    }
+
+    public static ClassResponse getClassResponse() {
+        return ClassResponse.builder()
+                .name("Class A")
+                .gradeYear(GradeYearEnum.EM_1ANO.getDescription())
+                .active(true)
+                .id(1l)
                 .build();
     }
 
